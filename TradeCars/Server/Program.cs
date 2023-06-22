fd709deba9c48d51efb694417334769bf694b45c
Patrick Szalapski <patrick.szalapski@genmills.com>
2023-06-21 23:10:34 -0500

    Removed more cruft for a more minimal example

--- TradeCars/Server/Program.cs
+++ TradeCars/Server/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Hosting.StaticWebAssets;
-using Microsoft.OpenApi.Models;
-
 namespace TradeCars.Server;
 
 public static class Program
@@ -8,18 +5,8 @@ public static class Program
     public static int Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Allow local static files to be hosted locally with an environment besides "development" - https://stackoverflow.com/a/74498969/7453
-        // TODO: how to do this in ASP.NET Core 7 to get rid of the compiler warning?  See https://stackoverflow.com/a/74498969/7453
-        builder.WebHost.ConfigureAppConfiguration((ctx, _) =>
-            StaticWebAssetsLoader.UseStaticWebAssets(ctx.HostingEnvironment, ctx.Configuration)
-        );
-
         builder.Services.AddControllersWithViews();
         builder.Services.AddRazorPages();
-
-        OpenApiInfo apiInfo = new() { Title = "TradeCars", Version = "v1", Description = "TradeCars API" };
-        builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", apiInfo));
 
         var app = builder.Build();
 
